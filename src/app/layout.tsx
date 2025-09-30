bb354f0bda6c4c78fa20e2f3c011fda26d10171e
Hartoooooo <hartmanntimon@gmail.com>
2025-09-30 08:55:05 +0200

    SEO: Optimierte Favicons für Google Search - 16x16, 32x32, ICO und Apple Touch Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
